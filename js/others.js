cdbbf8cc715bfb3af2f4d60024c3671429c3749f
topu <topukhan735@gmail.com>
2023-11-23 00:08:03 +0600

    Party mode added in colors

--- js/others.js
+++ js/others.js
@@ -6,3 +6,30 @@ changeColorDiv.addEventListener("click", (event) => {
         document.body.style.background = targetColor.id;
     }
 })
+
+let isPartyEnd = false
+
+const endParty = document.getElementById('partyStopBtn')
+endParty.addEventListener('click', () =>  {
+    partyStop()
+})
+
+const beginParty = document.getElementById('partyStartBtn')
+beginParty.addEventListener('click', () => {
+    isPartyEnd = false
+    if (isPartyEnd != true) {
+        partyStart()
+    }
+})
+
+function partyStart() {
+    let partyColor = ['red', 'green', 'blue', 'yellow', 'yellow', 'navy', 'purple', 'orange', 'lightblue', 'cyan', 'pink', 'brown', 'gray', 'black', 'white', 'olive', 'magenta', 'teal', 'maroon', 'indigo']
+    if (!isPartyEnd) {
+        let backgroundColor = Math.floor(Math.random() * partyColor.length)
+        document.body.style.background = partyColor[backgroundColor]
+        setTimeout(partyStart, 300)
+    }
+}
+function partyStop() {
+    isPartyEnd = true
+}
